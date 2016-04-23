308a4c2401c5889c70325ff84809cf0e1f2f406f
Webdev <Ian Simpson>
2016-04-23 13:59:41 -0700

    add snippet controller, resourcees in routes, index, new and show actions wip

---
+++ app/controllers/snippets_controller.rb
@@ -0,0 +1,36 @@
+class SnippetsController < ApplicationController
+
+  def index
+    snippets = Snippet.all
+  end
+
+  def show
+    @snippet = Snippet.find params[:id]
+  end
+
+  def new
+    @snippet = Snippet.new
+  end
+
+  def create
+    snippet_params = params.require(:snippet).permit(:title, :work)
+    @snippet = Snippet.new snippet_params
+    if @snippet.save
+      redirect_to @snippet, notice: "Snippet Added"
+    else
+      flash[:alert] = "Snippet was not saved..."
+      render :new
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
