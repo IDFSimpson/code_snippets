96b2941702ef4bf6965d6ad7dd003ea4107d021d
Webdev <Ian Simpson>
2016-04-23 17:17:08 -0700

    add a user login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:email, :password, :password_confirmation)
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Account created"
+    else
+      flash[:alert] = "Account creation failed"
+      render :new
+    end
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :kinds, only: [:index, :show]
   resources :snippets
 
+  resources :users, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
